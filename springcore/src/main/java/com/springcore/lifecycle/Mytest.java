3894d2014fd73449b33b52a558368acfbd66a359
Akash kumar Sahoo <akashnikz@gmail.com>
2021-09-15 12:43:50 +0530

    first

---
+++ springcore/src/main/java/com/springcore/lifecycle/Mytest.java
@@ -0,0 +1,18 @@
+package com.springcore.lifecycle;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Mytest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/lifecycle/config.xml");
+		Pepsi pepsi = (Pepsi) context.getBean("pepsi");
+		System.out.println(pepsi);
+		context.registerShutdownHook();
+
+	}
+
+}
